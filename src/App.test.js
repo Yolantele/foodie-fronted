e286eb251ea33ab83f9a24ea6e0ed15b64234606
cristhiandas <cristhiandf94@gmail.com>
2018-01-09 18:11:36 +0000

    created second test

--- src/App.test.js
+++ src/App.test.js
@@ -10,6 +10,7 @@ require('./setupTest.js');
 chai.use(chaiEnzyme());
 
 import App from './App';
+import Product from './Product/Product'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -18,4 +19,10 @@ it('renders without crashing', () => {
 
 it ('can use Sinon syntax', () => {
   expect(true).to.equal(true);
+});
+
+it ('has a header with App-header class', () => {
+  const mountWrapper = mount(<App />)
+  const shallowWrapper = shallow(<App />)
+  expect(mountWrapper.contains(<Product name='Pancake'/>)).to.equal(true)
 })
