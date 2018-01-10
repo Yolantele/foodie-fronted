0be3c37905c69dc8558828b6130df6fe30635cc0
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-10 12:11:20 +0000

    Refactor small

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Product from './Product/Product'
 class App extends Component {
   state = {
     products: [
-    {id:1, name: 'Pancake', quantity: '10', price: '5', date: '01/12/17'},
-    {id:2, name: 'p2', quantity: '11', price: '1', date: '02/12/17'},
-    {id:3, name: 'p3', quantity: '12', price: '2', date: '03/12/17'}]
+      {id:1, name: 'Pancake', quantity: '10', price: '5', date: '01/12/17'},
+      {id:2, name: 'p2', quantity: '11', price: '1', date: '02/12/17'},
+      {id:3, name: 'p3', quantity: '12', price: '2', date: '03/12/17'}
+    ]
   }
 
   render() {
     const products = this.state.products.map( (product) => {
-      return <Product name={product.name}
+      return <Product
+        name={product.name}
         quantity={product.quantity}
         price={product.price}
         date={product.date}
@@ -22,7 +24,7 @@ class App extends Component {
 
     return (
       <div className="App">
-      {products}
+        {products}
       </div>
     );
   }
