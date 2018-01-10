d438e9bcebbb6172abaa2a18f33e35e8d77f990c
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-10 12:44:20 +0000

    Integrating backend api -  JSON object to frontend React

--- src/App.js
+++ src/App.js
@@ -3,30 +3,41 @@ import './App.css';
 import Product from './Product/Product'
 
 class App extends Component {
+
   state = {
-    products: [
-      {id:1, name: 'Pancake', quantity: '10', price: '5', date: '01/12/17'},
-      {id:2, name: 'p2', quantity: '11', price: '1', date: '02/12/17'},
-      {id:3, name: 'p3', quantity: '12', price: '2', date: '03/12/17'}
-    ]
+    title: 'pancakes'
+  }
+  componentDidMount(){
+    fetch('http://localhost:5000/api/products')
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          productsData: data
+        })
+      })
   }
 
   render() {
-    const products = this.state.products.map( (product) => {
-      return <Product
-        name={product.name}
-        quantity={product.quantity}
-        price={product.price}
-        date={product.date}
-        key={product.id}
-        />
-    })
-
-    return (
-      <div className="App">
-        {products}
-      </div>
-    );
+    if (!this.state.productsData) {
+      return <p> Loading Products...</p>
+    } else {
+      const products = this.state.productsData.map( (product) => {
+        return <Product
+          name={product.name}
+          quantity={product.quantity}
+          price={product.price}
+          date={product.date}
+          key={product.id}
+          />
+      })
+      return (
+        <div className="App">
+          <p>Hello</p>
+          {products}
+        </div>
+      );
+    }
   }
 }
 
