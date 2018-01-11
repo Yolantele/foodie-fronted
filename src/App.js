9ab201ed2e8bf646707dbd5538a72cd50754768c
Tabish Raza <tabishraza@gmail.com>
2018-01-11 19:02:24 +0000

    filter for categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import Products from './components/Products/Products'
+import Category from './components/Products/Category/Category'
 
 class App extends Component {
 
-  state = {
-    title: 'pancakes'
+constructor(props){
+  super(props);
+  this.state = {
+    selection: [],
+    productsData: null,
+    dairySel: null
   }
+  // this.dairy = this.dairy.bind(this);
+}
+
   componentDidMount(){
     fetch('http://localhost:5000/api/products')
       .then(data => data.json())
       .then(data => {
-        console.log(data)
         this.setState({
           productsData: data
         })
       })
   }
 
+  filter = string => {
+    if (this.state.productsData) {
+      this.state.productsData.map( product => {
+        if(product.category === string){
+          this.state.selection.push(product);
+        };
+        this.setState({dairySel: this.state.selection});
+      });
+    } else {
+      return <p> No Dairy Products available</p>
+  }
+}
   render() {
     if (!this.state.productsData) {
       return <p> Loading Products...</p>
@@ -26,11 +45,21 @@ class App extends Component {
       const productsList = <Products
         products={this.state.productsData}
       />
+      const selectionList = <Products
+      products={this.state.selection}
+      />
 
+
+      // console.log("hasjdhsjak",this.state.dairySel)
       return (
         <div className="App">
           <p>Hello</p>
+          <button onClick={() => this.filter('dairy')}>Dairy</button>
+          <button onClick={() => this.filter('protein')}>Protein</button>
+
+          <a href="/auth/google">Sign in with Google</a>
           {productsList}
+          {selectionList}
         </div>
       );
     }
