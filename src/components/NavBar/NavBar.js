649c6ae7007ed49421a91670d7d152ac359fe9c5
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-12 11:05:42 +0000

    Navbar and Filter Product search form with bootstrap

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './NavBar.css';
 
+const pages = ['Foodie', 'Search', 'Google Login', 'Logout'];
+
 const NavBar = (props) => {
 
-  const pages = ['Foodie', 'Search', 'Google Login', 'Logout'];
   const navLinks = pages.map(page => {
     return (
-      <a href={'/' + page}>
-        {page}
-      </ a>
+      <div>
+        <a href={'/' + page}>
+          {page}
+        </ a>
+      </div>
     )
   });
-  return <nav>{navLinks}</nav>;
+  return (
+
+    <nav classNameName="NavBar">
+      <div classNameName="navbar navbar-expand-sm navbar-dark bg-primary mb-3">
+          <a classNameName="navbar-brand" href="/">Foodie</a>
+          {navLinks}
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar;
