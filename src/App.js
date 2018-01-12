255b78dd44c16733c33506db06936799baa43c37
Tabish Raza <tabishraza@gmail.com>
2018-01-12 11:22:18 +0000

    categories selection completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 // import NavBar from './components/NavBar/NavBar'
 import Products from './components/Products/Products'
 import SearchFilter from './components/Products/search-filter/SearchFilter'
+import ToggleDisplay from 'react-toggle-display';
 class App extends Component {
 
 constructor(props){
   super(props);
   this.state = {
     selection: [],
-    productsData: null,
-    dairySel: null
+    productsData: null
   }
 }
 
@@ -26,11 +26,12 @@ constructor(props){
 
   filter = string => {
     if (this.state.productsData) {
+      this.state.selection = [];
       this.state.productsData.map( product => {
-        if(product.category === string){
+        if(product.category === string || string === 'all'){
           this.state.selection.push(product);
         };
-        this.setState({dairySel: this.state.selection});
+        this.setState({filterSel: this.state.selection, hideList: !this.state.hideList});
       });
     } else {
       return <p> No Dairy Products available</p>
@@ -54,12 +55,15 @@ constructor(props){
       return (
         <div className="App">
           <p>Hello</p>
+          <button onClick={() => this.filter('all')}>All</button>
           <button onClick={() => this.filter('dairy')}>Dairy</button>
           <button onClick={() => this.filter('protein')}>Protein</button>
           {searchFilter}
           <a href="/auth/google">Sign in with Google</a>
-          {productsList}
           {selectionList}
+          <ToggleDisplay show={this.state.hideList}>
+            {productsList}
+          </ToggleDisplay>
         </div>
       );
     }
