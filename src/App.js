31b0bc03b13580f78d01df9d9346498ee337a0f5
Tabish Raza <tabishraza@gmail.com>
2018-01-12 09:09:12 +0000

    removed import for deleted file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Products from './components/Products/Products'
-import Category from './components/Products/Category/Category'
 
 class App extends Component {
 
